497dca8d7f8c4297b3d1efd501642ada175105ef
Darya_Savitskaya <statusqvogerz@gmail.com>
2022-09-07 00:23:36 +0500

    L2_Task4

---
+++ car.js
@@ -0,0 +1,21 @@
+const carName = 'BMV'
+let maxCarSpeed = '300 km/h'
+let carOwner = 'Darya'
+
+maxCarSpeed = '400 km/h'
+carOwner = 'Dmitriy'
+
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
+
+maxCarSpeed = '500 km/h'
+carOwner = 'Olga'
+
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
+
+maxCarSpeed = '200 km/h'
+carOwner = 'Ivan'
+
+console.log('У машины изменилась скорость', maxCarSpeed)
+console.log('У машины изменился владелец', carOwner)
